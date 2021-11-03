e8c24f4002b55b00e2dbfacc2bd5d024f10b12db
Olga Romaniko <oromaniko@gmail.com>
2021-11-03 15:09:34 +0300

    fix is-even.js

--- src/games/is-even.js
+++ src/games/is-even.js
@@ -4,16 +4,11 @@ import { getRandomInt } from '../random.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => {
-  if (num % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
+const isEven = (num) => num % 2 === 0;
 
 const getGameAnswerAndQuestion = () => {
   const question = getRandomInt(1, 100);
-  const answer = isEven(question);
+  const answer = isEven(question) === true ? 'yes' : 'no';
   return cons(question, answer);
 };
 
